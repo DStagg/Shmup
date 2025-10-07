9be7f045e5f99a5e5fe0fb8534e2edf754572efb
dstagg <staggy11@gmail.com>
2025-10-07 08:45:12 +0100

    Initialise SDL and associated top-level systems in Main.

--- Shmup/Main.cpp
+++ Shmup/Main.cpp
@@ -2,33 +2,67 @@
 	#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
 #endif
 
-#include <SFML/Graphics.hpp>
+#include <SDL3/SDL.h>
+#include <SDL3_mixer/SDL_mixer.h>
+#include <SDL3_ttf/SDL_ttf.h>
+
+#include "ext/Timer.h"
 
 #include "src/Scene.h"
 #include "src/MenuScene.h"
 
+SDL_Window* _Window = nullptr;
+SDL_Renderer* _Renderer = nullptr;
+MIX_Mixer* _Mixer = nullptr;
 
 int main()
 {
-	int FramesPerSecond = 60;
-	float deltaT = 0.f;
-	sf::Clock UpdateClock;
+	Timer UpdateClock;
 
-	sf::RenderWindow _Window;
-	_Window.create(sf::VideoMode(624, 624), "Shoot-em-up");
+	SDL_CreateWindowAndRenderer("Shoot-em-up", 624, 624, NULL, &_Window, &_Renderer);
+
+	if (!TTF_Init())
+	{
+		std::cout << "Couldn't init SDL_TTF :" << SDL_GetError() << std::endl;
+		return SDL_APP_FAILURE;
+	}
+	if (!MIX_Init())
+	{
+		const char* error = SDL_GetError();
+		std::cout << "Couldn't init SDL_Mixer :" << error << std::endl;
+		return SDL_APP_FAILURE;
+	}
+
+	_Mixer = MIX_CreateMixerDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, NULL);
+
+	if (!_Mixer)
+	{
+		const char* error = SDL_GetError();
+		std::cout << "Couldn't create mixer : " << error << std::endl;
+		return SDL_APP_FAILURE;
+	}
 
 	SceneManager SCM;
-	SCM.PushScene(new MenuScene(&_Window));
+	SCM.PushScene(new MenuScene(_Renderer));
 	
-	UpdateClock.restart();
+	UpdateClock.Reset();
 	while (SCM.GetActiveScenePntr() != 0)
 	{
 		SCM.GetActiveScenePntr()->Update(UpdateClock.restart().asSeconds());
-		_Window.clear();
+
+		SDL_SetRenderDrawColor(_Renderer, 0, 0, 0, 255);
+		SDL_RenderClear(_Renderer);
 		SCM.GetActiveScenePntr()->DrawScreen();
-		_Window.display();
+		SDL_RenderPresent(_Renderer);
 		SCM.CullScenes();
 	}
 	
-	return 0;
+	MIX_DestroyMixer(_Mixer);
+	MIX_Quit();
+	TTF_Quit();
+	SDL_DestroyRenderer(_Renderer);
+	SDL_DestroyWindow(_Window);
+	SDL_Quit();
+
+	return SDL_APP_SUCCESS;
 }
