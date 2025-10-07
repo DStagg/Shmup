2e25338089e72fcb77b1ace822d5412dc6e60074
dstagg <staggy11@gmail.com>
2025-10-07 10:43:17 +0100

    Implemented SDLAudioService.

--- Shmup/src/AudioService.cpp
+++ Shmup/src/AudioService.cpp
@@ -1,5 +1,107 @@
 #include "AudioService.h"
 
+SDLAudio::SDLAudio(MIX_Mixer* mix)
+{
+	if (!mix) mix = MIX_CreateMixerDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, NULL);
+	_Mixer = mix;
+};
+
+void SDLAudio::PlaySFX(std::string id)
+{
+	std::map<std::string, MIX_Audio*>::iterator result = _SFXs.find(id);
+	if (result != _SFXs.end())
+		MIX_PlayAudio(_Mixer, _SFXs[id]);
+};
+
+void SDLAudio::PlayStream(std::string id)
+{
+	std::map<std::string, MIX_Audio*>::iterator result = _Music.find(id);
+	if (result == _Music.end())
+		return;
+
+	for (std::vector<MIX_Track*>::iterator i = _Tracks.begin(); i != _Tracks.end(); i++)
+		if (MIX_GetTrackAudio(*i) == _Music[id])
+		{
+			if (MIX_TrackPaused(*i))
+				MIX_ResumeTrack(*i);
+			else
+			{
+				SDL_PropertiesID options = SDL_CreateProperties();
+				SDL_SetNumberProperty(options, MIX_PROP_PLAY_LOOPS_NUMBER, -1);
+				MIX_PlayTrack(*i, options);
+				SDL_DestroyProperties(options);
+			}
+			return;
+		}
+	
+	MIX_Track* mix = MIX_CreateTrack(_Mixer);
+	MIX_SetTrackAudio(mix, _Music[id]);
+	SDL_PropertiesID options = SDL_CreateProperties();
+	SDL_SetNumberProperty(options, MIX_PROP_PLAY_LOOPS_NUMBER, -1);
+	MIX_PlayTrack(mix, options);
+	SDL_DestroyProperties(options);
+	
+};
+
+void SDLAudio::StopSFX(std::string id)
+{
+	//	Not needed with SDL_Mixer.
+	std::cout << "Did you mean to call this? SFXs are fire-and-forget under SDL_Mixer." << std::endl;
+};
+
+void SDLAudio::StopStream(std::string id)
+{
+	for (std::vector<MIX_Track*>::iterator i = _Tracks.begin(); i != _Tracks.end(); i++)
+		if (MIX_GetTrackAudio(*i) == _Music[id])
+			MIX_StopTrack(*i,0);
+};
+
+void SDLAudio::PauseStream(std::string id)
+{
+	for (std::vector<MIX_Track*>::iterator i = _Tracks.begin(); i != _Tracks.end(); i++)
+		if (MIX_GetTrackAudio(*i) == _Music[id])
+			MIX_PauseTrack(*i);
+};
+
+bool SDLAudio::StreamPlaying(std::string id)
+{
+	for (std::vector<MIX_Track*>::iterator i = _Tracks.begin(); i != _Tracks.end(); i++)
+		if (MIX_GetTrackAudio(*i) == _Music[id])
+			return MIX_TrackPlaying(*i);
+	return false;
+};
+
+bool SDLAudio::StoreSFX(std::string id, std::string filename)
+{
+	MIX_Audio* sfx = nullptr;
+	sfx = MIX_LoadAudio(_Mixer, filename.c_str(), true);
+
+	if (!sfx) return false;
+
+	std::map<std::string, MIX_Audio*>::iterator result = _SFXs.find(id);
+	if (result != _SFXs.end())
+		MIX_DestroyAudio(result->second);
+	_SFXs[id] = sfx;
+
+	return true;
+};
+
+bool SDLAudio::StoreStream(std::string id, std::string filename)
+{
+	MIX_Audio* music = nullptr;
+	music = MIX_LoadAudio(_Mixer, filename.c_str(), true);
+
+	if (!music) return false;
+
+	std::map<std::string, MIX_Audio*>::iterator result = _Music.find(id);
+	if (result != _Music.end())
+		MIX_DestroyAudio(result->second);
+	_Music[id] = music;
+
+	return true;
+};
+
+/*
 SFMLAudio::SFMLAudio()
 {
 
@@ -65,3 +167,4 @@ bool SFMLAudio::StoreStream(std::string id, std::string filename)
 {
 	return _Streams[id].openFromFile(filename);
 };
+*/
--- Shmup/src/AudioService.h
+++ Shmup/src/AudioService.h
@@ -1,9 +1,36 @@
 #pragma once
 
-#include "Service.h"
+#include <SDL3_mixer/SDL_mixer.h>
+#include "core/Service.h"
 #include <map>
-#include "SFML\Audio.hpp"
+#include <vector>
+#include <iostream>
 
+class SDLAudio : public AudioService
+{
+public:
+	
+	SDLAudio(MIX_Mixer* mix = nullptr);
+
+	void PlaySFX(std::string id);
+	void PlayStream(std::string id);
+	void StopSFX(std::string id);
+	void StopStream(std::string id);
+	void PauseStream(std::string id);
+	bool StreamPlaying(std::string id);
+
+	bool StoreSFX(std::string id, std::string filename);
+	bool StoreStream(std::string id, std::string filename);
+
+protected:
+
+	std::vector<MIX_Track*> _Tracks;
+	std::map<std::string, MIX_Audio*> _SFXs;
+	std::map<std::string, MIX_Audio*> _Music;
+	MIX_Mixer* _Mixer;
+};
+//	TODO: Switch _Tracks to another map?
+/*
 class SFMLAudio : public AudioService
 {
 public:
@@ -26,3 +53,4 @@ protected:
 	std::map<std::string, sf::SoundBuffer> _SFXs;
 	std::map<std::string, sf::Music> _Streams;
 };
+*/
